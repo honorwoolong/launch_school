0946c41be6a23d31d14c7d10c046cc200a0d8c6f
Roy Wu <woolong0517@gmail.com>
2020-04-03 07:49:25 +0800

    Launch School Lesson 2 4/2/2020

--- test.rb
+++ test.rb
@@ -1,103 +1,23 @@
-def prompt(str)
-  puts "=> #{str}"
+require 'yaml'
+MESSAGE = YAML.load_file('test_message.yml') 
+# => {"en"=>{"welcome"=>"Welcome to Calculator! Enter your name:", "valid_name"=>"Make sure to enter a valid name."}, "es"=>{"welcome"=>"Bienvenido a la calculadora! Entre su nombre:", "valid_name"=>"Asegúrese de entrar un nombre válido."}}
+
+LANGUAGE = 'en'
+
+def messages(message, lang = 'en')
+  MESSAGE[lang][message]
 end
 
-def valid_number?(num)
-  num.to_i != 0
+def prompt(string)
+  message = messages(string, LANGUAGE)
+  Kernel.puts("=> #{message}")
 end
 
-def op_print(op)
-  case op
-  when "1"
-    "Adding"
-  when "2"
-    "Substracting"
-  when "3"
-    "Multiplying"
-  when "4"
-    "Dividing"
-  end
+prompt('welcome')
+
+def prompt_es(key)
+  message = messages(key, 'es')
+  puts "=> #{message}"
 end
 
-prompt("Welcome to Woolong Calculator!")
-
-  name = nil
-  loop do 
-    prompt("What's your name?")
-    name = gets.chomp
-    if name.empty?
-      prompt("Hmm...please provide it...")
-    else
-      break
-    end
-  end
-
-  prompt("Hi! #{name}!!")
-
-loop do 
-
-  num_1 = nil
-  loop do 
-    prompt("Give 1st number!")
-    num_1 = gets.chomp
-    if valid_number?(num_1)
-      break
-    else
-      prompt("It's not a valid number!")
-    end
-  end
-
-  num_2 = nil
-  loop do 
-    prompt("Give 2nd number!")
-    num_2 = gets.chomp
-    if valid_number?(num_2)
-      break
-    else
-      prompt("It's not a valid number!")
-    end
-  end
-
-  op = nil
-  operator_prompt = <<-MSG
-  Do you want to add, substract, multiply or divide?
-      1) Add
-      2) Substract
-      3) Multiply
-      4) Divide
-  MSG
-
-  loop do 
-    prompt(operator_prompt)
-    op = gets.chomp
-    if %w(1 2 3 4).include?(op)
-      break
-    else
-      promp("Please check your option of operator!")
-    end
-  end
-
-  result =  case op
-            when "1"
-              num_1.to_i + num_2.to_i
-            when "2"
-              num_1.to_i - num_2.to_i
-            when "3"
-              num_1.to_i * num_2.to_i
-            when "4"
-              num_1.to_f / num_2.to_f
-            end
-
-  prompt("#{op_print(op)} the two numbers...")
-  prompt("The result is #{result}!!")
-  
-  prompt("Do you want to continue?(Y to continue calculating)")
-  answer = gets.chomp
-  if answer.downcase.start_with?('y')
-    prompt("Let's keep going!")
-  else
-    break
-  end
-end
-
-prompt("Goodbye! My Love!")
+prompt_es('welcome')
